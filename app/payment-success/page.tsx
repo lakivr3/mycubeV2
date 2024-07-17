ccc3d1195f31032ab92aeb4a3e4d340768e8404a
lakivr3 <127746116+lakivr3@users.noreply.github.com>
2024-07-17 13:11:40 +0200

    page

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,8 +1,4 @@
-"use client";
-import { useGlobalContext } from "../context";
-
 export default function PaymentSuccess() {
-  const { total } = useGlobalContext();
   return (
     <div className="h-[100vh] ">
       <main className="  max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-green-500 to-green-800">
